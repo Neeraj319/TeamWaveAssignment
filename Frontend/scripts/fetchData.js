59c2b3d8b4c0c4ab48525bf61cfade909351850e
Neeraj319 <itskop520@gmail.com>
2022-07-03 13:45:16 +0545

    removed frontend from docker compose

---
+++ Frontend/scripts/fetchData.js
@@ -0,0 +1,13 @@
+
+const fetchFromAPI = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export default fetchFromAPI;
--- TeamWaveAssignment/settings.py
+++ TeamWaveAssignment/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "core.middleware.RequestThrottle",
+    # "core.middleware.RequestThrottle",
 ]
 
 ROOT_URLCONF = "TeamWaveAssignment.urls"
